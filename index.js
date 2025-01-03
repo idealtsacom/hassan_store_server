7bca5a99173a7132a01f153386ad1d21e235fcf0
Muhammad Hassan Ali <mh6467419@gmail.com>
2025-01-03 07:43:22 +0000

    updates

--- index.js
+++ index.js
@@ -25,17 +25,24 @@ const port = process.env.PORT || 5000;
 // Configure CORS
 app.use(
   cors({
-    origin: "https://hassan-store-client.vercel.app", // Client origin
+    origin: (origin, callback) => {
+      const allowedOrigins = ["http://localhost:3000", "https://hassan-store-client.vercel.app"];
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
     methods: ["GET", "POST", "PUT", "DELETE"],
-    credentials: true, // If cookies or authentication are involved
+    credentials: true,
   })
 );
-app.options("*", (req, res) => {
-  res.setHeader("Access-Control-Allow-Origin", "https://hassan-store-client.vercel.app");
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
-  res.status(200).end();
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
 });
+
 
 app.use(express.json());
 
